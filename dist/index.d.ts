28d2bf707e74797f49205a6213670a62b25267c3
Basal John <112621841+basal-john@users.noreply.github.com>
2024-12-05 20:15:13 +0100

    Remove dist and node_modules from repository and update .gitignore

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Reporter, TestCase, TestResult } from '@playwright/test/reporter';
-export default class CustomReporterConfig implements Reporter {
-    private failures;
-    private getRandomFailureQuote;
-    private getRandomSuccessQuote;
-    onTestEnd(test: TestCase, result: TestResult): void;
-    onEnd(): void;
-}
